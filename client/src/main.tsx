2fa5a88fb0fb7f8e9e00499896d63629e87c81e5
omkar date <omkardate29@gmail.com>
2023-08-09 23:12:34 +0530

    try different approach

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,3 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </ReduxProvider>
   </React.StrictMode>
 );
+
+// 1) try to replace redux store with react-query and recoil
+// 2) use another hooks like useMemo, useCallBackHook for better performance
