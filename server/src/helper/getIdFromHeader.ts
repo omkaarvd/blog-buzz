31322219875bcab31dcee49cf3e8301454ed5de1
Omkar Date <omkardate29@gmail.com>
2025-01-27 21:16:13 +0530

    clean

--- server/src/helper/getIdFromHeader.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const getIdFromHeader = (header: any) => {
-  if (!header) return Error('No header provided');
-  const decoded: any = jwt.verify(header, process.env.JWT_SECRET!);
-  return decoded.id;
-};
-
-export { getIdFromHeader };
