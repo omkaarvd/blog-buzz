d26ff814c7557c854929f291f083e3b5571f5b44
Omkar Date <omkardate29@gmail.com>
2025-01-27 22:57:17 +0530

    feat: add Prettier configuration and ignore files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
+    <main className="flex min-h-screen items-center justify-center">
       <h1>Blog Buzz</h1>
     </main>
   );
